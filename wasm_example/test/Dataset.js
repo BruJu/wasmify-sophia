0153fbd9e3579ef058b75508b1eab0b6847b9f33
Julian Bruyat <bruyat.ju@live.fr>
2020-02-25 11:40:48 +0100

    unit tests for diff, equals and intersection

--- wasm_example/test/Dataset.js
+++ wasm_example/test/Dataset.js
@@ -218,6 +218,194 @@ function runTests (rdf) {
       })
     })
 
+    describe('difference', () => {
+      it('should be a function', () => {
+        const dataset = rdf.dataset()
+
+        assert.strictEqual(typeof dataset.difference, 'function')
+      })
+
+      it('have no effect on empty datasets', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+
+        const graphSrc = rdf.dataset([quad11, quad12, quad13])
+        const graphDst = rdf.dataset()
+
+        const graphDiff = graphSrc.difference(graphDst);
+
+        assert.strictEqual(graphDiff.size, 3);
+        assert(graphDiff.has(quad11));
+        assert(graphDiff.has(quad12));
+        assert(graphDiff.has(quad13));
+      })
+
+      it('make an empty graph when differencing the same graph', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+
+        const graphSrc = rdf.dataset([quad11, quad12, quad13])
+
+        const graphDiff = graphSrc.difference(graphSrc);
+
+        assert.strictEqual(graphDiff.size, 0);
+      })
+
+      it('differentiate two different graphes', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+        const quad23 = rdf.quad(ex.subject2, ex.predicate, ex.object3)
+
+        const graphSrc = rdf.dataset([quad11, quad12, quad13])
+        const graphDst = rdf.dataset([quad11, quad23])
+
+        const graphDiff = graphSrc.difference(graphDst);
+
+        assert.strictEqual(graphDiff.size, 2);
+        assert(graphDiff.has(quad12));
+        assert(graphDiff.has(quad13));
+      })
+    })
+
+    describe('equals', () => {
+      it('should be a function', () => {
+        const dataset = rdf.dataset()
+
+        assert.strictEqual(typeof dataset.equals, 'function')
+      })
+
+      it('should be able to compare empty graphes', () => {
+        const graph1 = rdf.dataset()
+        const graph2 = rdf.dataset()
+
+        assert(graph1.equals(graph2))
+        assert(graph1.equals(graph1))
+        assert(graph2.equals(graph1))
+      })
+
+      it('should be able to compare two identical graphs', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+
+        const graph1 = rdf.dataset([quad11, quad12, quad13])
+        const graph2 = rdf.dataset([quad11, quad12, quad13])
+
+        assert(graph1.equals(graph2));
+        assert(graph1.equals(graph1));
+        assert(graph2.equals(graph1));
+      })
+
+      it('should be able to compare different graphes', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+
+        const graph1 = rdf.dataset([quad11, quad12])
+        const graph2 = rdf.dataset([quad13])
+        const empty_graph = rdf.dataset()
+
+        assert(!graph1.equals(graph2))
+        assert(!graph2.equals(graph1))
+        assert(!graph1.equals(empty_graph))
+        assert(!empty_graph.equals(graph1))
+      })
+
+      it('should have a different result when two graphes become identical', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+
+        const graph1 = rdf.dataset([quad11, quad12])
+        const graph2 = rdf.dataset([quad13])
+
+        assert(!graph1.equals(graph2))
+        graph2.add(quad11)
+        graph2.add(quad12)
+        graph1.add(quad13)
+        assert(graph1.equals(graph2))
+      })
+    })
+
+    describe('intersection', () => {
+      it('should be a function', () => {
+        const dataset = rdf.dataset()
+
+        assert.strictEqual(typeof dataset.intersection, 'function')
+      })
+
+      it('should be able to intersect empty graphes', () => {
+        const graph1 = rdf.dataset()
+        const graph2 = rdf.dataset()
+
+        assert.strictEqual(graph1.intersection(graph2).size, 0)
+        assert.strictEqual(graph1.intersection(graph1).size, 0)
+      })
+
+      it('should not modify the intersected graphes', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+        const graph1 = rdf.dataset([quad11, quad12])
+        const graph2 = rdf.dataset([quad13])
+
+        graph1.intersection(graph2)
+
+        assert(graph1.equals([quad11, quad12]))
+        assert(graph2.equals([quad13]))
+      })
+
+      it('should be able to intersect the same graph', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+
+        const graph1 = rdf.dataset([quad11, quad12, quad13])
+        const graph2 = rdf.dataset([quad11, quad12, quad13])
+
+        const intersection11 = graph1.intersection(graph1)
+        const intersection12 = graph1.intersection(graph2)
+
+        assert.strictEqual(graph1.size, 3)
+        assert(graph1.equals(intersection11))
+        assert(graph1.equals(intersection12))
+      })
+
+      it('should be able to intersect two disjoint graphes', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+        const quad14 = rdf.quad(ex.subject1, ex.predicate, ex.object4)
+
+        const graph1 = rdf.dataset([quad11, quad12])
+        const graph2 = rdf.dataset([quad13, quad14])
+
+        const intersection = graph1.intersection(graph2)
+
+        assert.strictEqual(intersection.size, 0)
+      })
+
+      it('should be able to intersect two graphes', () => {
+        const quad11 = rdf.quad(ex.subject1, ex.predicate, ex.object1)
+        const quad12 = rdf.quad(ex.subject1, ex.predicate, ex.object2)
+        const quad13 = rdf.quad(ex.subject1, ex.predicate, ex.object3)
+        const quad14 = rdf.quad(ex.subject1, ex.predicate, ex.object4)
+        const quadCommon = rdf.quad(ex.common, ex.come, ex.on)
+        const quadAnotherCommon = rdf.quad(ex.anothercommon, ex.come, ex.on)
+
+        const graph1 = rdf.dataset([quad11, quad12, quadCommon, quadAnotherCommon])
+        const graph2 = rdf.dataset([quad13, quad14, quadCommon, quadAnotherCommon])
+
+        const intersection = graph1.intersection(graph2)
+
+        assert.strictEqual(intersection.size, 2)
+        assert(intersection.equals(rdf.dataset([quadCommon, quadAnotherCommon])))
+      })
+
+    })
 
     describe('some', () => {
       it('should be a function', () => {
